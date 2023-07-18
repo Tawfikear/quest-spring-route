3e61d51c421cff9167906f872a97bd75dc27fc31
tawfik akaridan <tawfikear@gmail.com>
2023-07-18 13:07:16 +0200

    spring02

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -3,15 +3,21 @@ package com.wildcodeschool.doctor.controller;
 import com.wildcodeschool.doctor.model.Doctor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
-
+import com.wildcodeschool.doctor.model.Doctor;
 @Controller
 public class DoctorController {
-
-    @GetMapping("/doctor/")
+    
+    @GetMapping("/doctor{number}/")
     @ResponseBody
-    public Doctor doctor() {
+    public Doctor getDoctorByNumber(@PathVariable("number")String number) {
+        if (number.equals("13")){
+            Doctor doctor = new Doctor(13, "Jodie whittaker");
+            return doctor;
+        } else{ return null;
+        }
 
-        return null;
+        
     }
 }
