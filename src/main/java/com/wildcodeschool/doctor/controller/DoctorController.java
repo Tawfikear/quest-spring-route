778febcb49d81e23513687f43406f53f01d2a396
tawfik akaridan <tawfikear@gmail.com>
2023-07-19 23:14:41 +0200

    exercice terminé

--- src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
+++ src/main/java/com/wildcodeschool/doctor/controller/DoctorController.java
@@ -1,23 +1,28 @@
 package com.wildcodeschool.doctor.controller;
 
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 import com.wildcodeschool.doctor.model.Doctor;
+import org.springframework.web.server.ResponseStatusException;
+
 @Controller
 public class DoctorController {
-    
-    @GetMapping("/doctor{number}/")
-    @ResponseBody
-    public Doctor getDoctorByNumber(@PathVariable("number")String number) {
-        if (number.equals("13")){
-            Doctor doctor = new Doctor(13, "Jodie whittaker");
-            return doctor;
-        } else{ return null;
-        }
 
-        
+    @GetMapping("/doctor/{number}")
+    @ResponseBody
+    public Doctor getDoctorByNumber(@PathVariable("number") String number) {
+        int num = Integer.parseInt(number);
+        if (num == 13) {
+            Doctor doctor = new Doctor(13, "Jodie Whittaker");
+            return doctor;
+        } else if (num >= 1 && num <= 12) {
+            throw new ResponseStatusException(HttpStatus.SEE_OTHER, "error 303");
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Doctor Not Found");
+        }
     }
 }
